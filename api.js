364171ecb378656ddb54f3a39b9eaaacdc90e472
Neuber Oliveira <neuber.oliveira05@gmail.com>
2020-04-06 13:06:35 -0300

    group meals

--- api.js
+++ api.js
@@ -43,17 +43,66 @@ const fetchSchool = (code) => {
     historico: any[]
     */
 }
+const formatDate = (date) => {
+    const year = date.getFullYear()
+    const month = (date.getMonth() + 1).toString().padStart(2, '0')
+    const day = (date.getDate()).toString().padStart(2, '0')
+    return `${year}${month}${day}`
+}
 
 const fetchMenu = (date, schoolObject) => {
     const { tipo_unidade, tipo_atendimento, agrupamento, nome } = schoolObject
-    const year = date.getFullYear()
-    const month = (date.getMonth() + 1).toString().padStart(2, '0')
-    const day = (date.getDate() + 1).toString().padStart(2, '0')
-    const datestr = `${year}${month}${day}`
+    const datestr = formatDate(date)
     return fetch(`cardapios/${datestr}?tipo_unidade=${encodeURIComponent(tipo_unidade)}&tipo_atendimento=${encodeURIComponent(tipo_atendimento)}&agrupamento=${encodeURIComponent(agrupamento)}&nome=${encodeURIComponent(nome)}`)
+
+    /* 
+    tipo_atendimento:string
+    tipo_unidade: string
+    agrupamento: string|number
+    idade: string
+    data: string
+    data_publicacao: string
+    cardapio: {
+        Desjejum: string[]
+        Almoço: string[]
+        Lanche: string[]
+        Refeição da Tarde: string[]
+    }
+    */
+}
+
+const fetchMenuWeek = (date, schoolObject) => {
+    return new Promise((resolve, reject) => {
+        const promises = []
+        const result = {}
+        Array(5).fill(0).forEach(_ => {
+            promises.push(fetchMenu(date, schoolObject))
+
+            date.setDate(date.getDate() + 1)
+        })
+
+        Promise.all(promises).then((results) => {
+            resolve(results)
+        })
+    })
+}
+
+const groupWeeks = (results, ages) => {
+    const resp = {}
+    results.forEach(menuResp => menuResp.filter((item) => ages.indexOf(item.idade) !== -1).forEach(row => {
+        if (!resp[row.data]) {
+            resp[row.data] = {}
+        }
+
+        resp[row.data][row.idade] = row.cardapio
+    }))
+
+    return resp
 }
 
 module.exports = {
     fetchSchool,
     fetchMenu,
+    fetchMenuWeek,
+    groupWeeks,
 }
--- index.js
+++ index.js
@@ -1,24 +1,35 @@
 const api = require('./api')
+const requiredAges = ['2 a 3 anos', '1 ano']
+const requiredAges = ['2 a 3 anos', '1 ano']
+const meals = [
+    'Desjejum',
+    'Colação',
+    'Almoço',
+    'Lanche',
+    'Refeição da Tarde',
+]
 
 api.fetchSchool(309199).then(school => {
-    api.fetchMenu(calcMenuDate(), school).then(resp => {
-        console.log(resp)
+    // const startDate = calcMenuDate()
+    const startDate = new Date('2020-03-16 10:00:00')
+    api.fetchMenuWeek(startDate, school).then(resp => {
+        const mealsGroup = api.groupWeeks(resp, requiredAges)
+        Object.keys(mealsGroup).forEach(key => {
+            const group = mealsGroup[key]
+
+        })
     })
 })
-
-
-
-
 
 function calcMenuDate() {
     date = new Date()
     let weekday = date.getDay()
-    if (weekday >= 1 && weekday <= 5) {
+    if (weekday >= 1 && weekday <= 5) {// monday to friday
         const subDays = weekday - 1 //-1 is to prune calculation to get always the monday of the current week
         date.setDate(date.getDate() - subDays)
-    } else if (weekday == 6) {
-        date.setDate(date.getDate() + 6)
-    } else if (weekday == 0) {
+    } else if (weekday == 6) {// saturday
+        date.setDate(date.getDate() + 2)
+    } else if (weekday == 0) {// sunday
         date.setDate(date.getDate() + 1)
     }
 
